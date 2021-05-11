9b753115cbd40885e2c0a91b39d6e3adfc15ee39
Gornovaya <79137339+Gornovaya@users.noreply.github.com>
2021-05-11 21:43:36 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,99 +4,107 @@
 #include <cassert>
 
 template <typename T>
-class TQueue {
- private:
-  T *arr;     // массив с данными
-  int size;   // максимальное количество элементов в очереди (размер массива)
-  int begin,  // начало очереди
-      end;    // конец очереди
-  int count;  // счетчик элементов
- public:
-  TQueue(int = 100);  // конструктор по умолчанию
-  ~TQueue();          // деструктор
+class TPQueue {
+private:
+    T* arr;
+    int size;
+    int begin, end;
+    int count;
 
-  void push(const T &);  // добавить элемент в очередь
-  T pop();               // удалить элемент из очереди
-  T get() const;         // прочитать первый элемент
-  bool isEmpty() const;  // пустая ли очередь?
-  bool isFull() const;   // заполнен ли массив?
+public:
+    TPQueue();
+    ~TPQueue();
+    void push(const T&);
+    T pop();
+    T get() const;
+    bool isEmpty() const;
+    bool isFull() const;
 };
 
 // конструктор по умолчанию
 template <typename T>
-TQueue<T>::TQueue(int sizeQueue)
-    : size(sizeQueue), begin(0), end(0), count(0) {
-  // дополнительный элемент поможет нам различать конец и начало очереди
-  arr = new T[size + 1];
+TPQueue<T>::TPQueue()
+    : size(100)
+    , begin(0)
+    , end(0)
+    , count(0)
+{
+    arr = new T[size + 1];
 }
 
 // деструктор класса Queue
 template <typename T>
-TQueue<T>::~TQueue() {
-  delete[] arr;
+TQueue<T>::~TQueue()
+{
+    delete[] arr;
 }
 
 // функция добавления элемента в очередь
 template <typename T>
-void TQueue<T>::push(const T &item) {
-  // проверяем, ести ли свободное место в очереди
-  assert(count < size);
-  if (end != 0) {
-    arr[end] = item;
-    for (int i = end - 1; i >= begin; i--) {
-      if (item.prior > arr[i].prior) {
-        arr[i + 1] = arr[i];
-        arr[i] = item;
-      }
+void TPQueue<T>::push(const T& item)
+{
+    assert(count < size);
+    if (end != 0) {
+        for (int i = end - 1; i > -1; --i) {
+            if (arr[i].prior >= item.prior) {
+                arr[i + 1] = item;
+                continue;
+            }
+            if (arr[i].prior < item.prior) {
+                arr[i + 1] = arr[i];
+                if (i == 0)
+                    arr[i] = item;
+                continue;
+            }
+        }
     }
-  } else {
-    arr[begin] = item;
-  }
-  end++;
-  count++;
-
-  // проверка кругового заполнения очереди
-  if (end > size) end -= size + 1;  // возвращаем end на начало очереди
+    else {
+        arr[begin] = item;
+    }
+    count++;
+    end++;
 }
 
 // функция удаления элемента из очереди
 template <typename T>
-T TQueue<T>::pop() {
-  // проверяем, есть ли в очереди элементы
-  assert(count > 0);
-
-  T item = arr[begin++];
-  count--;
-
-  // проверка кругового заполнения очереди
-  if (begin > size) begin -= size + 1;  // возвращаем begin на начало очереди
-
-  return item;
+T TQueue<T>::pop()
+{
+    // проверяем, есть ли в очереди элементы
+    assert(count > 0);
+    T item = arr[begin++];
+    count--;
+    // проверка кругового заполнения очереди
+    if (begin > size)
+        begin -= size + 1; // возвращаем begin на начало очереди
+    return item;
 }
 
 // функция чтения элемента на первой позиции
 template <typename T>
-T TQueue<T>::get() const {
-  // проверяем, есть ли в очереди элементы
-  assert(count > 0);
-  return arr[begin];
+T TQueue<T>::get() const
+{
+    // проверяем, есть ли в очереди элементы
+    assert(count > 0);
+    return arr[begin];
 }
 
 // функция проверки очереди на пустоту
 template <typename T>
-bool TQueue<T>::isEmpty() const {
-  return count == 0;
+bool TQueue<T>::isEmpty() const
+{
+    return count == 0;
 }
 
 // функция проверки очереди на заполненность
 template <typename T>
-bool TQueue<T>::isFull() const {
-  return count == size;
+bool TQueue<T>::isFull() const
+{
+    return count == size;
 }
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
 };
 
-#endif  // INCLUDE_TPQUEUE_H_
+#endif // INCLUDE_TPQUEUE_H_
